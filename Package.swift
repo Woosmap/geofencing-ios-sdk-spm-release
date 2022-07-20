9bc844a62b0f8e71c98a09165fb1b3b4e88eff38
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2022-07-20 12:03:51 +0530

    Woosmap/geofencing-enterprise-ios-sdk#10 Collected swift package script

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "1.0.16"
+let moduleName = "WoosmapGeofencing"
+let checksum = "8ccc1444d9f4ccd20eb1b9e3de18c9cc2070d853c7968d80cfa9600bb79eca6a"
+
+let package = Package(
+    name: moduleName,
+    platforms: [
+        .iOS(.v13) //, .macCatalyst(.v15), .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: moduleName,
+            targets: [moduleName]
+        ),
+        .library(
+            name: "\(moduleName)dependencies",
+            targets: ["\(moduleName)dependencies"]
+        )
+    ],
+    dependencies: [
+        // Surge Package
+        .package(url: "https://github.com/Jounce/Surge.git", from: "2.3.0"),
+        // Realm
+        .package(url: "https://github.com/realm/realm-cocoa", from: "10.5.1")
+    ],
+    targets: [
+        .target(
+            name: "\(moduleName)dependencies",
+            dependencies: [
+                .product(name: "RealmSwift", package: "realm-cocoa"),
+                .product(name: "Surge", package: "Surge"),
+            ],
+            path: "dependencies"
+        ),
+        .binaryTarget(
+            name: moduleName,
+            url: "https://github.com/Woosmap/geofencing-ios-sdk-spm-release/releases/download/\(version)/\(moduleName).xcframework.zip",
+            checksum: checksum
+        )
+    ]
+)
