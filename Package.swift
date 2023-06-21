c518dc2e0c291354d3f342efe09020acbb30b539
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2023-06-21 13:57:56 +0530

    Revert type of library

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
     products: [
         .library(
             name: moduleName,
-            type: .dynamic,
             targets: [moduleName]
         ),
 //        .library(
