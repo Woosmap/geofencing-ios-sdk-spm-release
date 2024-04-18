34f649d066ad546e98e59e53072d0e85221e9bb3
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2024-04-18 10:22:55 +0530

    bump version: 4.3.3

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let version = "4.3.2"
+let version = "4.3.3"
 let moduleName = "WoosmapGeofencing"
-let checksum = "24f3f9e7bce891c21afc18f5850bcd4c84a592a32c6af550ae8c12cb75388076"
+let checksum = "5ded0112a55c51350c352b91bd050d318e7aecbf4baf5d34dd66a9e766cfc311"
 
 let package = Package(
     name: moduleName,
